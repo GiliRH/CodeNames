76bb4b9e3ce6f01f69dee682a5ca0f39f2df8083
GiliRH <113033626+GiliRH@users.noreply.github.com>
2023-05-12 13:19:09 +0300

    Add files via upload

--- gamebase.py
+++ gamebase.py
@@ -4,64 +4,87 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 
-
-class LeaderGrid:
+class Card:
     def __init__(self):
-        self.enemy_guessed = 0
-        self.left2guess = 0
-        # in the grid:
-        # 0 - regular (should be 25-8-8-1 = 8)
-        # 1 - own (8)
-        # 2 - enemy (8)
-        # 3 - spy (1)
-        self.grid = [0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0]
+        # 0 - neutral (should be 25-8-8-1 = 8): white/yellow
+        # 1 - blue (8): team color
+        # 2 - red (8): team color
+        # 3 - spy (1): black
+        self.known = 0
+        self.type = 0
+        self.word = ""
 
-        self.words = ["", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", ""]
+class Grid:
+    def __init__(self):
+        # 5x5
+        self.grid = [Card(), Card(), Card(), Card(), Card(),
+                     Card(), Card(), Card(), Card(), Card(),
+                     Card(), Card(), Card(), Card(), Card(),
+                     Card(), Card(), Card(), Card(), Card(),
+                     Card(), Card(), Card(), Card(), Card()]
+
+    def __str__(self):
+        st = "words:"
+        for c in self.grid:
+            st += c.word + ', '
+        st += '\ntypes:'
+        for t in self.grid:
+            st = st + str(t.type) + ','
+        return st
 
     def set_board(self):
-        pass
+        pos = []
+        for i in range(len(self.grid)):
+            pos.append(i)
+
+        for b in range(8): # blues
+            p = random.choice(pos)
+            self.grid[p].type = 1
+            pos.remove(p)
+
+        for r in range(8): # reds
+            p = random.choice(pos)
+            self.grid[p].type = 2
+            pos.remove(p)
+
+        p = random.choice(pos) #spy
+        self.grid[p].type = 3
+        pos.remove(p)
 
 
+    def import_random_words(self):
+        words_list = get_words_list()
+        for i in range(len(self.grid)):
+            n = random.randint(0,len(words_list) -1)
+            self.grid[i].word = words_list[n]
+            words_list.remove(words_list[n])
 
-class GuesserGrid:
-    def __init__(self):
-        # in the grid:
-        # 0 - regular (should be 25-8-8-1 = 8)
-        # 1 - own (8)
-        # 2 - enemy (8)
-        # 3 - spy (1)
-        # 4 - unknown
-        self.grid = [4, 4, 4, 4, 4,
-                     4, 4, 4, 4, 4,
-                     4, 4, 4, 4, 4,
-                     4, 4, 4, 4, 4,
-                     4, 4, 4, 4, 4]
-        self.words = ["", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", "",
-                      "", "", "", "", ""]
 
-    def set_board(self):
-        pass
 
     def make_guess(self):
         pass
 
 
 def get_words_list():
-    pass
+    """
+    This function saves all the optional words for the game
+    and saves them into a list
+    """
+    with open("words.txt", "r") as f:
+        words_list = f.read()
+        words_list = words_list.split()
+    return words_list
 
 
-def import_random_words():
-    pass
+
+def main():
+    words_list = get_words_list()
+    grid = Grid()
+    grid.import_random_words()
+    grid.set_board()
+    print(grid)
+
+if __name__ == "__main__":
+    main()
 
 
